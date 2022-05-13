f245f4ea9327b05565808d57470795b9386dc66c
Sefa <49004028+Doringl@users.noreply.github.com>
2022-05-13 16:19:01 +0300

    Create Slug.ts

---
+++ Slug/Slug.ts
@@ -0,0 +1,25 @@
+const Slug = (text: string): string => {
+  const trMap = {
+    çÇ: "c",
+    ğĞ: "g",
+    şŞ: "s",
+    üÜ: "u",
+    ıİ: "i",
+    öÖ: "o",
+  };
+
+  for (let key in trMap) {
+    text = text.replace(
+      new RegExp("[" + key + "]", "g"),
+      trMap[key as keyof typeof trMap]
+    );
+  }
+
+  return text
+    .replace(/[^-a-zA-Z0-9\s]+/gi, "") // remove non-alphanumeric chars
+    .replace(/\s/gi, "-") // convert spaces to dashes
+    .replace(/[-]+/gi, "-") // trim repeated dashes
+    .toLowerCase();
+};
+
+export default Slug;
